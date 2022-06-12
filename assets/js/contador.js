d78ee9cfe779906888f0465ec339e4aa7689d0b3
Drolpg <leandronascimento.drolpg21@gmail.com>
2022-06-12 20:43:30 -0300

    Start project

---
+++ assets/js/contador.js
@@ -0,0 +1,36 @@
+let count_casa = 0;
+let count_fora = 0;
+
+
+const CURRENT_NUMBER = document.getElementById('currentNumber');
+
+function increment_casa() {
+	count_casa++;
+	CURRENT_NUMBER.innerHTML = count_casa;
+}
+
+function decrement_casa() {
+	count_casa--;
+	CURRENT_NUMBER.innerHTML = count_casa;
+}
+
+function test() {
+	kdowkdpo;
+}
+
+
+const CURRENT_NUMBER2 = document.getElementById('currentNumber2');
+
+function increment_fora() {
+	count_fora++;
+	CURRENT_NUMBER2.innerHTML = count_fora;
+}
+
+function decrement_fora() {
+	count_fora--;
+	CURRENT_NUMBER2.innerHTML = count_fora;
+}
+
+function test_fora() {
+	kdowkdpo;
+}
